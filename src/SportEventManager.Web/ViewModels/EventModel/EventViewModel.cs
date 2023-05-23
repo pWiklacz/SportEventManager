bc9eada9dad3f85b080a8742d6b8c0cba352b323
Piotr Głąb <piotgla495@student.polsl.pl>
2023-05-23 04:29:20 +0200

    * SEM-6 Event creation, deletation and details showing works now with the new db, * SEM-6 Automatic match generation for 2^n different teams.

--- src/SportEventManager.Web/ViewModels/EventModel/EventViewModel.cs
+++ src/SportEventManager.Web/ViewModels/EventModel/EventViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using SportEventManager.Core.EventAggregate;
 using SportEventManager.Web.ViewModels.TeamModel;
 
 namespace SportEventManager.Web.ViewModels.EventModel;
@@ -6,14 +7,45 @@ namespace SportEventManager.Web.ViewModels.EventModel;
 public class EventViewModel
 {
   public int Id { get; set;}
+  public string OwnerId { get; private set; } = string.Empty;
   public string Name { get; set;} = string.Empty;
+
+  public List<MatchViewModel> Matches { get; set; } = new List<MatchViewModel>();
   public List<StadiumViewModel> Stadiums { get; set;} = new List<StadiumViewModel>();
   public List<TeamViewModel> Teams { get; set; } = new List<TeamViewModel>();
-  public List<string> TeamsName { get; set; } = new();
-  public List<string> SelectTeamsName { get; set; } = new();
-  public List<MatchViewModel> Matches { get; set; } = new List<MatchViewModel>();
+  public List<string> AvailableTeamsNames { get; set; } = new List<string>();
+  public List<string> ChosenTeamsNames { get; set; } = new List<string>();
 
   [DisplayName("Start Time")]
-  public DateTime StartTime { get; set; }
+  public DateTime StartTime { get; set; } = DateTime.Now;
+
+  [DisplayName("End Time")]
+  public DateTime EndTime { get; set; } = DateTime.Now;
   public bool IsArchived { get; set; } = false;
+
+  public bool IsInprogress { get; set; } = false;
+
+  public static EventViewModel FromEvent(Event @event)
+  {
+    return new EventViewModel
+    {
+      Id = @event.Id,
+      OwnerId = @event.OwnerId,
+      Name = @event.Name,
+      IsArchived = @event.IsArchived,
+      IsInprogress = @event.IsInprogress,
+      StartTime = @event.StartTime,
+      EndTime = @event.EndTime,
+      Matches = @event.Matches.Select(m => MatchViewModel.FromMatch(m)).ToList(),
+      Stadiums = @event.Stadiums.Select(s => StadiumViewModel.FromStadium(s)).ToList(),
+      Teams = @event.Teams.Select(t => TeamViewModel.FromTeam(t)).ToList()
+    };
+  }
+
+  public EventViewModel()
+  {
+    Stadiums.Add(new StadiumViewModel() { Id = 1 });
+    Matches.Add(new MatchViewModel() { Id = 1 });
+    ChosenTeamsNames.Add("default");
+  }
 } 
