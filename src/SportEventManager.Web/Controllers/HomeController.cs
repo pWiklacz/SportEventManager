664982ad546d2287eeb21dd1619f9b6818348af4
Piotr Głąb <piotgla495@student.polsl.pl>
2023-06-01 01:51:55 +0200

    Changed conditions for events displaying in home view

--- src/SportEventManager.Web/Controllers/HomeController.cs
+++ src/SportEventManager.Web/Controllers/HomeController.cs
@@ -3,25 +3,11 @@ using SportEventManager.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using SportEventManager.Core.EventAggregate;
 using SportEventManager.SharedKernel.Interfaces;
-using SportEventManager.Core.TeamAggregate;
 using SportEventManager.Web.ViewModels.EventModel;
-using SportEventManager.Web.ViewModels.TeamModel;
-using Microsoft.Extensions.Logging;
-using SportEventManager.Core.UserAggregate;
-using Microsoft.AspNetCore.Identity;
-using JetBrains.Annotations;
 using SportEventManager.Core.EventAggregate.Specifications;
-using Ardalis.Specification;
-using System.Linq;
 
 namespace SportEventManager.Web.Controllers;
 
-/// <summary>
-/// A sample MVC controller that uses views.
-/// Razor Pages provides a better way to manage view-based content, since the behavior, viewmodel, and view are all in one place,
-/// rather than spread between 3 different folders in your Web project. Look in /Pages to see examples.
-/// See: https://ardalis.com/aspnet-core-razor-pages-%E2%80%93-worth-checking-out/
-/// </summary>
 public class HomeController : Controller
 {
   private readonly IRepository<Event> _eventRepository;
@@ -36,7 +22,7 @@ public class HomeController : Controller
   {
     var sportEvents = new List<Event>();
 
-    EventWithItemsSpec eventWithItemsSpec = new EventWithItemsSpec();
+    EventsWithItemsSpec eventWithItemsSpec = new EventsWithItemsSpec();
     sportEvents = await _eventRepository.ListAsync(eventWithItemsSpec);
 
     var dto = new List<EventViewModel>();
