3a56b3542fd6f63ef0743b25d301de203789d389
Piotr Głąb <piotgla495@student.polsl.pl>
2023-06-07 03:37:05 +0200

    Changed the organization of Program.cs so we would initialize example data in other place

--- src/SportEventManager.Core/UserAggregate/User.cs
+++ src/SportEventManager.Core/UserAggregate/User.cs
@@ -1,10 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Identity;
-using SportEventManager.Core.EventAggregate;
 using SportEventManager.SharedKernel.Interfaces;
-using SportEventManager.Core.TeamAggregate;
 
 namespace SportEventManager.Core.UserAggregate;
 public class User : IdentityUser, IAggregateRoot
